12f704480f3d999c7da2df1a08bdac1415962da7
akifrt <91149807+akifrt@users.noreply.github.com>
2021-12-15 09:59:49 +0400

    .

---
+++ homework 13.12.2021/1/JS/index.js
@@ -0,0 +1,36 @@
+document.querySelector('.button-Green').onclick = function changeColor(){
+	
+	document.querySelector('.color-green').style.backgroundColor = "green";
+	document.querySelector('.color-purple').style.backgroundColor = "";
+	document.querySelector('.color-red').style.backgroundColor = "";
+	document.querySelector('.color-orange').style.backgroundColor = "";
+	
+}
+
+document.querySelector('.button-Orange').onclick = function changeColor(){
+	
+	document.querySelector('.color-orange').style.backgroundColor = "Orange";
+	document.querySelector('.color-purple').style.backgroundColor = "";
+	document.querySelector('.color-red').style.backgroundColor = "";
+	document.querySelector('.color-green').style.backgroundColor = "";
+	
+}
+
+document.querySelector('.button-Red').onclick = function changeColor(){
+	
+	document.querySelector('.color-red').style.backgroundColor = "red";
+	document.querySelector('.color-green').style.backgroundColor = "";
+	document.querySelector('.color-orange').style.backgroundColor = "";
+	document.querySelector('.color-purple').style.backgroundColor = "";
+	
+}
+
+document.querySelector('.button-Purple').onclick = function changeColor(){
+	
+	
+	document.querySelector('.color-purple').style.backgroundColor = "purple";
+	document.querySelector('.color-red').style.backgroundColor = "";
+	document.querySelector('.color-green').style.backgroundColor = "";
+	document.querySelector('.color-orange').style.backgroundColor = "";
+	
+}
